53ed33eabeb1c45f97806a789c1f8d1b6b2b04c0
alxbtvsky <alxbtvsky@gmail.com>
2015-02-04 11:08:28 -0500

    Level4

--- js/app.js
+++ js/app.js
@@ -1,13 +1,18 @@
 (function() {
   var app = angular.module('gemStore', []);
-
-  app.controller('GalleryController', function(){
+app.directive('productGallery', function(){
+    return {
+    restrict: 'E',
+    templateUrl: 'product-gallery.html',
+    controller: function(){
     this.current = 0;
     this.setCurrent = function(imageNumber){
       this.current = imageNumber || 0;
+    };  
+    },
+    controllerAs: 'gallery'
     };
-  });
-
+});
   
 
   app.controller('StoreController', function() {
